01c05e5262d6d4b5859cfb23b445402576af4a67
qat47621 <qat47621@gmail.com>
2020-11-21 15:14:31 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const test = document.querySelector('.test');
+
+function addItem(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(x, y);
+
+    const item = document.createElement('div');
+    
+    item.style.top = y + 'px';
+    item.style.left = x + 'px';
+    item.style.transform = 'translate(-50%, -50%)';
+    item.classList.add('item');
+    // console.log(item.style.top, item.style.top);
+
+    test.appendChild(item);
+}
+
+test.addEventListener('click', addItem);
